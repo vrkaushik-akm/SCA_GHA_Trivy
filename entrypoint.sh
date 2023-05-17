04a2b0aa4370167f515be1f71bf4bd771647d2d8
vrkaushik-akm <125484876+vrkaushik-akm@users.noreply.github.com>
2023-05-17 11:52:55 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#gets trivy version
+trivy --version
+
+#generates an sbom for the current repo suing trivy
+trivy repo --format cyclonedx --scanners vuln --output trivy.json $INPUT_REPO_URL
+
+#uploads the sbom for the given repo to dtrack
+curl -X POST https://testdtrack.vrkaushik.com/api/api/v1/bom -H "accept: application/json" -H "X-API-Key: ${{ secrets.DTRACK_API_KEY }}" -H "Content-Type: multipart/form-data" -F "autoCreate=true" -F "projectName=$INPUT_REPO" -F "bom=@trivy.json" 
+
+#cleanup 
+rm trivy.json
